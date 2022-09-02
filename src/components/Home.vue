a7c62569f300211cef61e9cf6efc14668932713c
Toni Kolarić <toni14nexe@gmail.com>
2022-09-02 11:58:16 +0200

    Added all components for main page and styles.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-
+    <center>
+      <component v-bind:is="Component"/>
+      <about/>
+      <dishes/>
+      <find-us/>
+      <footer-component/>
+    </center>
   </div>
 </template>
 
 
 
 <script>
+import Head from "./Head.vue"
+import AboutUs from './About.vue'
+import Dishes from './Dishes.vue'
+import FindUs from './FindUs.vue'
+import Footer from './Footer.vue'
+import LoggedOut from '../assets/loggedOut.js'
 
 export default {
   data(){
     return{
+      Component: 'header',
     }
+  },
+  components:{
+    'header': Head,
+    'about': AboutUs,
+    'dishes': Dishes,
+    'find-us': FindUs,
+    'footer-component': Footer
+  },
+  mounted(){
+    LoggedOut.check();
   }
 }
 </script>
@@ -19,5 +42,7 @@ export default {
 
 
 <style scoped>
-
+  #app{
+    background-color: black;
+  }
 </style>
